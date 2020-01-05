34dd2af8c72d624ebd9154b7baaced00d802a692
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-01-05 14:33:56 +0100

    updated test

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,18 +4,25 @@ source bashrc
 
 set -e
 
+docker image ls
 docker-present-build
+docker image ls
 
+docker container ls -a
 echo 1 | docker-present
-sleep 5
 docker container ls -a
 docker-present-stop
+docker container ls -a
 
 docker-present-ansible-compatibility-testing
+docker container ls -a
 docker-present-stop ansible-compatibility-testing
+docker container ls -a
 
 docker-present-publish-revealjs
+docker container ls -a
 
 docker-present-publish ansible-compatibility-testing
+docker container ls -a
 
 set +e
