246d706c1f37d6956737d8ed8133ca88a69a53e9
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-01-08 13:59:25 +0100

    updated test.sh

--- .ci/test.sh
+++ .ci/test.sh
@@ -19,6 +19,11 @@ docker container ls -a
 docker-present-stop ansible-compatibility-testing
 docker container ls -a
 
+docker-present-redhat-forum-2020
+docker container ls -a
+docker-present-stop redhat-forum-2020
+docker container ls -a
+
 docker-present-publish-revealjs
 docker container ls -a
 
