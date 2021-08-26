7d95db61806921a7ea6b78050f26166f69eeab6a
Clemens Kaserer <ckaserer@gmail.com>
2021-08-26 17:16:34 +0200

    restructured repository

--- make.sh
+++ make.sh
@@ -6,7 +6,6 @@
 
 readonly PROG_NAME=`basename "$0"`
 readonly SCRIPT_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-readonly TOP_LEVEL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)
 
 #################### 
 # GLOBAL VARIABLES #
@@ -16,20 +15,20 @@ readonly TOP_LEVEL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)
 # SOURCE #
 ##########
 
-docker build -t presentations ${TOP_LEVEL_DIR}/revealjs
+docker build -t presentations .
 docker container rm -f presentation >/dev/null
 docker run -d --entrypoint=/opt/revealjs/bin/present.py --name presentation presentations Training-for-Containerization 8080
-docker cp presentation:/opt/revealjs ${TOP_LEVEL_DIR}/docs
+docker cp presentation:/opt/revealjs docs/
 
-for presentation in ${TOP_LEVEL_DIR}/revealjs/src/presentations/*; do
+for presentation in src/presentations/*; do
     echo $(basename $presentation)
     docker container rm -f presentation >/dev/null
     docker run -d --entrypoint=/opt/revealjs/bin/present.py --name presentation presentations $(basename $presentation) 8080
-    docker cp presentation:/opt/revealjs/index.html ${TOP_LEVEL_DIR}/docs/$(basename $presentation).html
+    docker cp presentation:/opt/revealjs/index.html docs/$(basename $presentation).html
 done
 
 docker container rm -f presentation >/dev/null
 docker run -d --entrypoint=/opt/revealjs/bin/present.py --name presentation presentations index 8080
-docker cp presentation:/opt/revealjs/index.html ${TOP_LEVEL_DIR}/docs/index.html
+docker cp presentation:/opt/revealjs/index.html docs/index.html
 
-rm -rf ${TOP_LEVEL_DIR}/docs/{.git,.github}
+rm -rf docs/{.git,.github}
